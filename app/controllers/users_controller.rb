4b4d9a82ed06a46499e9758cb008aa11109e3225
jimhantrix <jimtekanyo@gmail.com>
2019-03-31 18:35:56 +0800

    created a user create params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
 
-
 	def show 
 		@users = User.friendly.find(params[:id])
 	end
@@ -10,5 +9,9 @@ class UsersController < ApplicationController
 		render :new 
 	end 
 
+	def create 
+
+		user = User.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], params[:email], phone: params[:phone], country: params[:country], address: params[:address], password: params[:password])
+		redirect_to_users_path
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,20 @@
 Rails.application.routes.draw do
 
-root "users#index"
+	root "users#index"
 
-get "/users" => "users#index", as: "users"
+	get "/users" => "users#index", as: "users"
 
-get "/users" => "users#new", as: "new_user"
+	get "/users" => "users#new", as: "new_user"
 
-get "/signup" => "user#new"
+	get "/signup" => "user#new"
 
-post "/users" => "users#create"
-
-
-
-get "/login" => "sessions#new"
-
-get "/login" => "sessions#create"
-
-get "/logout" => "sessions#destroy"
+	post "/users" => "users#create"
 
 
 
+	get "/login" => "sessions#new"
 
+	get "/login" => "sessions#create"
 
+	get "/logout" => "sessions#destroy"
 end
