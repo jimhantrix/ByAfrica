d60563fad53974df586b0d9b0d8d8207b3031e70
jimhantrix <jimtekanyo@gmail.com>
2019-04-01 15:10:24 +0800

    updated user avatar

---
+++ db/migrate/20190401070803_add_attachment_avatar_to_users.rb
@@ -0,0 +1,13 @@
+class AddAttachmentAvatarToUsers < ActiveRecord::Migration[5.2]
+  
+
+  def up
+    change_table :users do |t|
+      t.attachment :avatar
+    end
+  end
+
+  def down
+    remove_attachment :users, :avatar
+  end
+end
