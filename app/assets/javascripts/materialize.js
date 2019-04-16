9a73edd43ff7349abc04f5e3a96bfbe242229594
jimhantrix <jimtekanyo@gmail.com>
2019-04-16 22:38:58 +0800

    updated layouts page for application

--- app/assets/javascripts/materialize.js
+++ app/assets/javascripts/materialize.js
@@ -1,10 +1,8 @@
   
+ var instance = M.Carousel.getInstance(elem);
 
-   var instance = M.Carousel.getInstance(elem);
-
-   document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, options);
+var instance = M.Carousel.init({
+    fullWidth: true,
+    indicators: true
   });
-
    
